26b74b1e946330b7ee4056030f54f6377409ac95
Cliff Ingham <inghamn@bloomington.in.gov>
2020-03-09 14:02:08 -0400

    Use custom format for pg_dump
    Dump a seperate dump file for each database.  Use the custom format for pg_dump.
    
    Updates #8

--- templates/backup.sh
+++ templates/backup.sh
@@ -5,18 +5,31 @@ BACKUP_DIR=/srv/backups/postgres
 
 # How many days worth of tarballs to keep around
 num_days_to_keep=5
-
-#----------------------------------------------------------
-# Backups
-#----------------------------------------------------------
 now=`date +%s`
 today=`date +%F`
 
-cd $BACKUP_DIR
-sudo -u postgres pg_dumpall -c > ${today}.sql
-gzip ${today}.sql
+#----------------------------------------------------------
+# Create a binary backup for each database
+#----------------------------------------------------------
+[ ! -d $BACKUP_DIR/$today ] && mkdir $BACKUP_DIR/$today
 
+cd $BACKUP_DIR/$today
+sudo -u postgres pg_dumpall -g > roles.sql
+
+databases=$(sudo -u postgres psql -Xtc "select datname from pg_database where datistemplate=false")
+for d in $databases
+do
+    if [ ! $d = postgres ]; then
+        sudo -u postgres pg_dump -F c $d > $d.dump
+    fi
+done
+cd ..
+tar czvf $today.tar.gz $today
+rm -Rf $today
+
+#----------------------------------------------------------
 # Purge any backup tarballs that are too old
+#----------------------------------------------------------
 cd $BACKUP_DIR
 for file in `ls`
 do
